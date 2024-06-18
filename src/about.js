93e03f4966e2eeefd500052983aac16764e03416
BrickBoss97 <unknownyoungster@gmail.com>
2024-06-18 15:20:10 -0700

    Add individual page .js files and linked to index.js

---
+++ src/about.js
@@ -0,0 +1,3 @@
+const loadAbout = () => {};
+
+export default loadAbout;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./style.css";
+import loadHome from "./home.js";
+import loadMenu from "./menu.js";
+import loadAbout from "./about.js";
+
+const buttonListener = () => {
+	document.getElementById("home").addEventListener("click", loadHome);
+	document.getElementById("menu").addEventListener("click", loadMenu);
+	document.getElementById("about").addEventListener("click", loadAbout);
+};
+
+loadHome();
+buttonListener();
