3f6f975db37aaa4d0830b9ed826cb25895853c96
BrickBoss97 <unknownyoungster@gmail.com>
2024-06-19 20:59:13 -0700

    Add page build to Home.js with necessary style elements

--- src/home.js
+++ src/home.js
@@ -1,3 +1,32 @@
-const loadHome = () => {};
+const loadHome = () => {
+	const contentDiv = document.querySelector(".content");
+
+	contentDiv.innerHTML = "";
+
+	const createSection = (title, content) => {
+		const container = document.createElement("div");
+		container.classList.add("conatiner");
+		contentDiv.append(container);
+
+		const sectionHeader = document.createElement("div");
+		sectionHeader.classList.add("section-header");
+		sectionHeader.textContent = title;
+		container.append(sectionHeader);
+
+		const sectionContent = document.createElement("div");
+		sectionContent.classList.add("section-content");
+		sectionContent.textContent = content;
+		container.append(sectionContent);
+	};
+
+	createSection(
+		"Who We Are:",
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget gravida cum sociis natoque. Morbi leo urna molestie at elementum. Arcu risus quis varius quam."
+	);
+	createSection(
+		"What We Do:",
+		"Sed velit dignissim sodales ut eu sem. Felis eget nunc lobortis mattis aliquam faucibus purus in. Egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Sed viverra ipsum nunc aliquet bibendum."
+	);
+};
 
 export default loadHome;
--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,13 @@
-const loadMenu = () => {};
+const loadMenu = () => {
+	const contentDiv = document.querySelector(".content");
+
+	contentDiv.innerHTML = "";
+
+	const createSection = (title, content) => {
+		const container = document.createElement("div");
+		container.classList.add("conatiner");
+		contentDiv.append(container);
+	};
+};
 
 export default loadMenu;
